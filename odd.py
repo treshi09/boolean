247ac4386e5a42810bacf155fe45a7bd5a91a98c
Your Name <you@example.com>
2025-07-31 17:40:36 +0100

    commit

---
+++ odd.py
@@ -0,0 +1,15 @@
+#Write a program to check the application of not equal operator.
+
+num=int(input("Enter any number to check if it is even or odd: "))
+if num % 2 != 0:
+    print(f"{num} is an odd number");
+else:
+    print(f"{num} is an even number");
+
+a="Treasure"
+b="Catherine"
+c="Careen"
+if (a==b):
+    print("Hello")
+if (b!=c):
+    print("B and C are different")
